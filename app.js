561012b7af6454e6500d3d162c760192344c37af
Win Kyaw Kyaw <wk.k@mit.com.mm>
2025-02-10 09:33:24 +0630

    git attribute

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const personRoutes = require("./routes/person");
 const admin = require("firebase-admin");
+const fs = require("fs");
 
-// Initialize Firebase Admin SDK
-const serviceAccount = require("./firebase-admin-sdk.json");
+// Initialize Firebase Admin SDK using environment variable
+const serviceAccount = JSON.parse(fs.readFileSync(process.env.GOOGLE_CLOUD_KEY_PATH, 'utf8'));
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
 });
